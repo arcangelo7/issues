1f7c45c4facbf0a2132f5bbf64f8100d2256f570
arcangelo7 <arcangelomas@gmail.com>
2022-09-16 21:26:29 +0200

    createdAt

--- process_issues.py
+++ process_issues.py
@@ -80,7 +80,7 @@ def store(issue_title:str, issue_body:str, created_at:str, user_id:int):
 
 
 if __name__ == "__main__":
-    output = subprocess.run(["gh", "issue", "list", "--state", "open", "--label", "deposit", "--json", "title,body,number,author,created_at"], capture_output=True, text=True)
+    output = subprocess.run(["gh", "issue", "list", "--state", "open", "--label", "deposit", "--json", "title,body,number,author,createdAt"], capture_output=True, text=True)
     print(output)
     # for issue in issues:
     #     is_valid, message = validate(issue["title"], issue["body"])

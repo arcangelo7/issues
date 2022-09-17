40bd24fb1e718d2a299c8e7900c6311f44c35739
arcangelo7 <arcangelomas@gmail.com>
2022-09-17 11:59:32 +0200

    deposit_on_zenodo

--- process_issues.py
+++ process_issues.py
@@ -15,14 +15,18 @@
 # SOFTWARE.
 
 
+from datetime import datetime
 from pandas import read_csv
-from typing import Tuple
+from typing import List, Tuple
 import csv
 import io
 import json
 import oc_idmanager
+import os
 import re
+import requests
 import subprocess
+import time
 
 
 def __validate_title(title:str) -> Tuple[bool, str]:
@@ -50,7 +54,7 @@ def validate(issue_title:str, issue_body:str) -> Tuple[bool, str]:
         split_data = issue_body.split("===###===@@@===")
         read_csv(io.StringIO(split_data[0].strip()))
         read_csv(io.StringIO(split_data[1].strip()))
-        return True, "Thank you for your contribution! OpenCitations will process the data you provided within a week. Afterwards, citations will be available on the [CROCI](https://opencitations.net/index/croci) index and metadata on OpenCitations Meta"
+        return True, "Thank you for your contribution! OpenCitations just processed the data you provided. The citations will soon be available on the [CROCI](https://opencitations.net/index/croci) index and metadata on OpenCitations Meta"
     except Exception:
         return False, "The data you provided could not be processed as a CSV. Please, check that the metadata CSV and the citation CSV are valid CSVs"
 
@@ -62,21 +66,78 @@ def answer(is_valid:bool, message:str, issue_number:str) -> None:
     subprocess.run(["gh", "issue", "edit", issue_number, "--add-label", label])
     subprocess.run(["gh", "issue", "close", issue_number, "--comment", message])
 
-def store(issue_title:str, issue_body:str, created_at:str, user_id:int):
+def __get_user_id(username:str) -> str:
+    tentative = 3
+    while tentative:
+        tentative -= 1
+        try:
+            r = requests.get(f"https://api.github.com/users/{username}", headers={"Accept":"application/vnd.github+json"}, timeout=30)
+            if r.status_code == 200:
+                r.encoding = "utf-8"
+                json_res = json.loads(r.text)
+                return json_res.get("id")
+        except requests.ReadTimeout:
+            # Do nothing, just try again
+            pass
+        except requests.ConnectionError:
+            # Sleep 5 seconds, then try again
+            time.sleep(5)
+
+def get_data_to_store(issue_title:str, issue_body:str, created_at:str, username:str) -> dict:
     split_data = issue_body.split("===###===@@@===")
     metadata = list(csv.DictReader(io.StringIO(split_data[0].strip())))
     citations = list(csv.DictReader(io.StringIO(split_data[1].strip())))
-    new_object = {
-        'data': {
-            'title': issue_title,
-            'metadata': metadata,
-            'citations': citations
+    user_id = __get_user_id(username)        
+    return {
+        "data": {
+            "title": issue_title,
+            "metadata": metadata,
+            "citations": citations
         },
-        'provenance': {
-            'generatedAtTime': created_at,
-            'wasAttributedTo': user_id
+        "provenance": {
+            "generatedAtTime": created_at,
+            "wasAttributedTo": user_id
         }
-    }    
+    }
+
+def __create_deposition_resource(today:str) -> str:
+    r = requests.post("https://zenodo.org/api/deposit/depositions",
+        params={"access_token": os.environ["TOKENS"]["ZENODO"]},
+        json={"metadata": {
+            "upload_type": "dataset",
+            "publication_date": today,
+            "title": f"OpenCitations crowdsourcing: deposits of the week before {today}",
+            "creators": [{"name": "Massari, Arcangelo", "affiliation": "Research Centre for Open Scholarly Metadata, Department of Classical Philology and Italian Studies, University of Bologna, Bologna, Italy", "orcid": "0000-0002-8420-0696"}],
+            "description": f"OpenCitations collects citation data and related metadata from the community through issues on the GitHub repository https://github.com/opencitations/crowdsourcing. In order to preserve long-term provenance information, such data is uploaded to Zenodo every week. This upload contains the data of deposit issues published in the week before {today}.",
+            "access_right": "open",
+            "license": "CC0-1.0",
+            "prereserve_doi": True,
+            "keywords": ["OpenCitations", "crowdsourcing", "provenance", "GitHub issues"],
+            "related_identifiers": [{"isDerivedFrom": "https://github.com/opencitations/crowdsourcing"}],
+            "version": "1.0.0"
+        }},
+        headers={"Content-Type": "application/json"})
+    print(r.json())
+    return r.json()["links"]["bucket"]
+
+def __upload_data(today:str, bucket:str) -> None:
+    with open("data_to_store.json", "rb") as fp:
+        r = requests.put(
+            "%s/%s" % (bucket, f"{today}_weekly_deposit"),
+            data=fp,
+            params={"access_token": os.environ["TOKENS"]["ZENODO"]}
+        )
+    r.json()
+
+def deposit_on_zenodo(data_to_store:List[dict]) -> None:
+    with open('data_to_store.json', 'w') as outfile:
+        json.dump(data_to_store, outfile)
+    today = datetime.now().strftime("%Y-%m-%d")
+    bucket = __create_deposition_resource(today)
+    __upload_data(today, bucket)
+    # r = requests.post('https://zenodo.org/api/deposit/depositions/%s/actions/publish' % deposition_id,
+    #                     params={'access_token': ACCESS_TOKEN} )
+    # print(r.json())
 
 
 if __name__ == "__main__":
@@ -85,6 +146,14 @@ if __name__ == "__main__":
         "--json", "title,body,number,author,createdAt"], 
         capture_output=True, text=True)
     issues = json.loads(output.stdout)
+    data_to_store = list()
     for issue in issues:
-        is_valid, message = validate(issue["title"], issue["body"])
-        answer(is_valid, message, str(issue["number"]))
+        issue_title = issue["title"]
+        issue_body = issue["body"]
+        issue_number = str(issue["number"])
+        created_at = issue["createdAt"]
+        username = issue["author"]
+        is_valid, message = validate(issue_title, issue_body)
+        answer(is_valid, message, issue_number)
+        data_to_store.append(get_data_to_store(issue_title, issue_body, created_at, username))
+    deposit_on_zenodo(data_to_store)
